eaa59f9a06f8037487bbae427d339207829a8d75
thibaut88 <code.thibaut@gmail.com>
2016-11-14 20:43:07 +0100

    logout dynamique

--- scripts/logout.php
+++ scripts/logout.php
@@ -1,8 +1,6 @@
 <?php
-
 session_start();
 session_unset();
 session_destroy();
-
-
-header('Location:../index.php');
+$location=str_replace('logout.php','',$_SERVER['SCRIPT_NAME']).'../index.php';
+header("Location:$location");
