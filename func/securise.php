aa3e92d15f97fbabeed84062b946383ac73c6512
thibaut88 <code.thibaut@gmail.com>
2016-11-14 17:49:01 +0100

    ajout func securite formulaire

---
+++ func/securise.php
@@ -0,0 +1,12 @@
+	<?php
+	
+	
+	//FUNC SECURISER FORMULAIRE
+	function securise($var)
+	{ 
+		$var = stripslashes($var); 
+		$var = htmlentities($var); 
+		$var = strip_tags($var); 
+		return $var;
+	}
+	?>
