32881fdfc4364ef27767c21b1b62f111ef3c3adc
thibaut88 <code.thibaut@gmail.com>
2016-11-16 16:32:03 +0100

    add inc form_add_video

--- admin/pages/form_add_video.php
+++ admin/pages/form_add_video.php
@@ -1,30 +1,26 @@
-		<div class="container">
+		<div class="container" >
 			<div class="row">
 				<div class="col-sm-6 col-sm-offset-3">
-						<h1 class="title">ajouter une vidéo</h1>
 
 
 
-						<form action="videos.php" method="post" enctype="multipart/form-data">
+						<form class="animated shake" action="videos.php" method="post" enctype="multipart/form-data" style="margin-top:20px;">
 
 								<div class="form-group">
 										<label for="">Titre:</label>
 										<input type="text" name="titre" placeholder=""class="form-control" id="" required>
 								</div>
-
 								<div class="form-group">
 										<label for="">Url (embed format):</label>
-										<input type="text" name="url" placeholder=""class="form-control" id=""required>
+										<input type="text" name="url"  placeholder="http://www/embed/" class="form-control" id=""required>
 								</div>
-
 								<div class="form-group">
 										<label for="">Auteur:</label>
 										<input type="text" name="auteur" placeholder=""class="form-control" id=""required>
 								</div>
-
 								<div class="form-group">
 									  <label for="categorie">Catégorie:</label>
-									  <select class="form-control" id="categorie" name="categorie"required>
+									  <select class="form-control" id="categorie" name="categorie"required style="background:orange;">
 										<option value="0">Choisissez une catégorie</option>
 										<?php
 										$sql = "SELECT * FROM categories";
@@ -38,13 +34,13 @@
 									  </select>
 								</div>
 								<div class="form-group">
-										<label for="">Vignette:</label>
-										<input type="button" value="vignette format image"class="form-control" id="vignetteclick" onclick="document.getElementById('vignette').click();">
+										<label for="">Vignette Image:</label>
+										<input style="background:orange;"type="button" value="Choisir une image"class="form-control" id="vignetteclick" onclick="document.getElementById('vignette').click();">
 										<input type="file" name="vignette" placeholder=""class="form-control" id="vignette"style="display:none;">
 								</div>
-							<div class="form-group">
+								<div class="form-group">
 										<label for="">Vignette URL:</label>
-										<input type="url" name="vignette_url" placeholder="http://www/embed/"class="form-control" id="vignette_url">
+										<input type="url" name="vignette_url"class="form-control" id="vignette_url">
 								</div>							
 								<div class="form-group">
 										<textarea name="description" style="width:100%;max-width:100%;" rows=10 required> </textarea> 
@@ -53,9 +49,8 @@
 								<div class="form-group">
 										<label class="checkbox-inline"><input type="checkbox" class="checkbox" name="publication"> Mettre en ligne ?</label>
 								</div>
-
 								
-										<input type="submit"name="sendAjouter"value="ajouter" class="btn btn-success">
+								<input type="submit"name="sendAjouter"value="ajouter" class="btn btn-success btn-md">
 						</form><!-- END Form -->
 
 
