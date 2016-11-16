2a441a06ae9f0115246bb7a1d6250c317b11cd4f
thibaut88 <code.thibaut@gmail.com>
2016-11-16 16:34:36 +0100

    add script delete_video

---
+++ admin/js/delete_video.js
@@ -0,0 +1,15 @@
+function deleteVideo(id_video){
+	
+		$.ajax({
+		type:"POST",
+		url:"../ajax/delete_video.php",
+		data:'id_video='+id_video,
+		dataType:'html',
+		success:function(result){
+				$("#menuVideo").after(result);
+					alert('ok');
+		}
+	
+
+	})
+}
