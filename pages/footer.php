de4a48e8ee5566d5039f404bf58be585f18e1b20
thibaut88 <code.thibaut@gmail.com>
2016-11-20 22:58:20 +0100

    add footer empty

---
+++ pages/footer.php
@@ -0,0 +1,24 @@
+<footer>
+
+
+<div class="container">
+<div class="row">
+
+<div class="col-xs-4">
+<h1 class="animated zoomIn">
+<span class="well">VIDEOALL</span>
+</h1>
+</div>
+<div class="col-xs-4">colonne</div>
+<div class="col-xs-4">colonne</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+</footer>
